8c9228d8d3e489c86b0b956dd543602575cb6000
Roger M. Nabinger <roger.nabinger@gmail.com>
2015-12-10 14:57:57 +0100

    - Implementierung Spinner in activity_edit.xml - Implementierung div. Methoden in EditActivity.java - TODO in EditControllerInterf.java - Eintraege fuer Spinner in strings.xml eingefuegt

--- app/src/main/java/com/example/thomasroehl/shopadminandroid/edit/EditControllerInterf.java
+++ app/src/main/java/com/example/thomasroehl/shopadminandroid/edit/EditControllerInterf.java
@@ -6,6 +6,7 @@ import java.sql.Date;
  * Created by Thomas Roehl on 04.12.2015.
  */
 public interface EditControllerInterf {
+    // TODO: Thomas - statt saveData wäre hier besser editData, da Speichern erst im editView erfolgt.
     public boolean saveData(String shop, String category, double sum, Date date);
     public String screenFlowCamera();
     public String screenFlowMain();
