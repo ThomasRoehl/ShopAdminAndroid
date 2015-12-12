e553500ece421482c3f5a1699f0383bc3b2a9d3a
Sascha <mail@saschazepf.net>
2015-12-12 17:15:38 +0100

    Changed Interfaces for Report and MainView

--- app/src/main/java/com/example/thomasroehl/shopadminandroid/reports/ReportControllerInterf.java
+++ app/src/main/java/com/example/thomasroehl/shopadminandroid/reports/ReportControllerInterf.java
@@ -1,11 +1,18 @@
 package com.example.thomasroehl.shopadminandroid.reports;
 
+import android.content.Context;
+import android.content.Intent;
+import android.widget.TableLayout;
+
 /**
  * Created by Thomas Roehl on 04.12.2015.
  */
 public interface ReportControllerInterf {
-    public String screenFlowMain();
+    public void setTableLayout(TableLayout tableLayout);
+    public void setCurrentActivityContext(Context context);
+
+    public Intent screenFlowMain();
     public boolean setFilter(String filter);
-    public Object getTable();
-    public Object getTable(String name);
+    public void getTable(String name);
+
 }
--- app/src/main/java/com/example/thomasroehl/shopadminandroid/startscreen/StartScreenControllerInterf.java
+++ app/src/main/java/com/example/thomasroehl/shopadminandroid/startscreen/StartScreenControllerInterf.java
@@ -1,11 +1,16 @@
 package com.example.thomasroehl.shopadminandroid.startscreen;
 
+import android.content.Context;
 import android.content.Intent;
+import android.widget.TableLayout;
 
 /**
  * Created by Thomas Roehl on 04.12.2015.
  */
 public interface StartScreenControllerInterf {
+    public void setTableLayout(TableLayout tableLayout);
+    public void setCurrentActivityContext(Context context);
+
     public Intent screenFlowScan();
     public Intent screenFlowTable();
     public void getTable(int amount);
